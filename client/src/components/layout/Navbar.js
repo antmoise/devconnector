aad07fa771205f7ca293024d07b053dd6b2b35ad
Antonio Moise <antmoise@hotmail.com>
2019-02-07 15:53:45 -0800

    Changed a tag to Link

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -15,8 +15,8 @@ class Navbar extends Component {
     const authLinks = (
       <ul className="navbar-nav ml-auto">
         <li className="nav-item">
-          <a
-            href=""
+          <Link
+            to=""
             onClick={this.onLogoutClick.bind(this)}
             className="nav-link"
           >
@@ -28,7 +28,7 @@ class Navbar extends Component {
               title="You must have a Gravatar connected to your email to display an image"
             />{" "}
             Logout
-          </a>
+          </Link>
         </li>
       </ul>
     );
