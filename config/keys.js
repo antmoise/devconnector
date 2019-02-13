77539ed562e50343daa5ef4ba1f1197bb68da385
Antonio Moise <antmoise@hotmail.com>
2019-02-13 14:37:12 -0800

    Securing Our Keys

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
-module.exports = {
-  mongoURI: "mongodb://ace:Password123@ds030500.mlab.com:30500/devconnector",
-  secretOrKey: "secret"
-};
+if (process.env.NODE_ENV === "production") {
+  module.exports = require("./keys_prod");
+} else {
+  module.exports = require("./keys_dev");
+}
